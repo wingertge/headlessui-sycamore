be9ebbd8f5cf5d70bc031b47548ff809a5e4a4c5
Genna Wingert <wingertge@gmail.com>
2023-01-24 19:34:09 +0100

    Update base example

--- examples/.base/src/main.rs
+++ examples/.base/src/main.rs
@@ -8,5 +8,7 @@ fn App<G: Html>(cx: Scope) -> View<G> {
 }
 
 pub fn main() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+
     sycamore::render(|cx| view! { cx, App {} })
 }
