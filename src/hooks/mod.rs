c35795b3e7df0413d84cfc9e58486bba0fc6634c
Genna Wingert <wingertge@gmail.com>
2023-02-13 21:30:28 +0100

    Use scyamore builtin ID generator for IDs

--- src/hooks/mod.rs
+++ src/hooks/mod.rs
@@ -1,4 +1,4 @@
-use std::sync::atomic::{AtomicU32, Ordering};
+/* use std::sync::atomic::{AtomicU32, Ordering};
 
 static ID: AtomicU32 = AtomicU32::new(0);
 
@@ -8,4 +8,17 @@ pub fn create_id() -> String {
 
 pub fn generate_id() -> u32 {
     ID.fetch_add(1, Ordering::Relaxed)
+}
+ */
+
+use sycamore::stable_id::create_unique_id;
+
+pub fn create_id() -> String {
+    let id = create_unique_id();
+
+    if let (Some(component_id), item_id) = id {
+        format!("headlessui-sycamore-{component_id}-{item_id}")
+    } else {
+        format!("headlessui-sycamore-{}", id.1)
+    }
 }
