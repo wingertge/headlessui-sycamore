f0fc1c4f1c8bf091fb8151d29c9df686c4506c3a
Genna Wingert <wingertge@gmail.com>
2023-02-19 23:10:19 +0100

    Add transition example and fix bugs

--- examples/transition/src/main.rs
+++ examples/transition/src/main.rs
@@ -1,14 +1,31 @@
+use headlessui_sycamore::components::Transition;
 use sycamore::prelude::*;
 
 #[component]
 fn App<G: Html>(cx: Scope) -> View<G> {
+    let show = create_signal(cx, false);
+
     view! { cx,
-        "Hello World"
+        p { (format!("{}", show)) }
+        button(on:click = |_| show.set(true), class = "p-1 m-1 border-gray-400 border") { "Open" }
+        button(on:click = |_| show.set(false), class = "p-1 m-1 border-gray-400 border") { "Close" }
+        Transition(
+            show = show,
+            enter = "transition-opacity duration-1000",
+            enter_from = "opacity-0",
+            enter_to = "opacity-100",
+            leave = "transition-opacity duration-1000",
+            leave_from = "opacity-100",
+            leave_to = "opacity-0",
+        ) {
+            "Hello World"
+        }
     }
 }
 
 pub fn main() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    wasm_logger::init(wasm_logger::Config::default());
 
     sycamore::render(|cx| view! { cx, App {} })
 }
--- src/components/transition.rs
+++ src/components/transition.rs
@@ -1,18 +1,27 @@
 use std::borrow::Cow;
 #[cfg(target_arch = "wasm32")]
-use std::mem;
+use std::{cell::RefCell, mem, rc::Rc};
 
 #[cfg(target_arch = "wasm32")]
 use crate::utils::oneshot_event;
-use crate::utils::{get_ref, SetDynAttr};
+use crate::{
+    hooks::create_id,
+    utils::{get_ref, SetDynAttr},
+};
+#[cfg(target_arch = "wasm32")]
+use js_sys::Array;
 #[cfg(target_arch = "wasm32")]
 use sycamore::web::html::ev;
 use sycamore::{builder::prelude::div, prelude::*};
 use sycamore_utils::{DynamicElement, ReactiveBool, ReactiveStr};
 #[cfg(target_arch = "wasm32")]
-use wasm_bindgen::{prelude::Closure, JsCast};
+use wasm_bindgen::prelude::Closure;
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::{JsCast, JsValue, UnwrapThrowExt};
 #[cfg(target_arch = "wasm32")]
 use web_sys::Window;
+#[cfg(target_arch = "wasm32")]
+use web_sys::{Element, Node};
 
 pub type TransitionProp<'cx, G> =
     Box<dyn FnOnce(BoundedScope<'_, 'cx>, &'cx ReadSignal<bool>) -> View<G> + 'cx>;
@@ -75,6 +84,38 @@ pub struct TransitionContext<G: Html> {
     pub node_ref: &'static NodeRef<G>,
 }
 
+#[cfg(target_arch = "wasm32")]
+fn add_class(node: &Node, class: &str) {
+    let class_list = class.split_ascii_whitespace().collect::<Vec<_>>();
+    if class_list.len() == 1 {
+        node.unchecked_ref::<Element>()
+            .class_list()
+            .add_1(class_list[0])
+            .unwrap_throw();
+    } else {
+        node.unchecked_ref::<Element>()
+            .class_list()
+            .add(&class_list.into_iter().map(JsValue::from).collect::<Array>())
+            .unwrap_throw();
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+fn remove_class(node: &Node, class: &str) {
+    let class_list = class.split_ascii_whitespace().collect::<Vec<_>>();
+    if class_list.len() == 1 {
+        node.unchecked_ref::<Element>()
+            .class_list()
+            .remove_1(class_list[0])
+            .unwrap_throw();
+    } else {
+        node.unchecked_ref::<Element>()
+            .class_list()
+            .remove(&class_list.into_iter().map(JsValue::from).collect::<Array>())
+            .unwrap_throw();
+    }
+}
+
 #[component]
 pub fn Transition<'cx, G: Html>(cx: Scope<'cx>, props: TransitionProps<'cx, G>) -> View<G> {
     let visible = create_signal(cx, props.show.get());
@@ -99,107 +140,128 @@ pub fn Transition<'cx, G: Html>(cx: Scope<'cx>, props: TransitionProps<'cx, G>) 
 
     let node = get_ref(cx, &props.attributes);
 
+    let id = create_id();
     #[cfg(target_arch = "wasm32")]
-    let mut initial = true;
+    let selector = create_ref(cx, format!("[data-sh-transition-id=\"{id}\"]"));
 
     #[cfg(target_arch = "wasm32")]
-    let mut transition = move |element: &DomNode, should_enter: bool, window: Window| {
-        if should_enter {
-            if initial {
-                let Properties {
-                    enter,
-                    enter_from,
-                    enter_to,
-                    entered,
-                    before_enter,
-                    after_enter,
-                    ..
-                } = properties;
+    let enter = move |element: &DomNode, window: Window| {
+        let Properties {
+            enter,
+            enter_from,
+            enter_to,
+            entered,
+            before_enter,
+            after_enter,
+            ..
+        } = properties;
 
-                let end_transition = move || {
-                    element.remove_class(enter);
-                    element.remove_class(enter_to);
-                    element.add_class(entered);
+        let closure = Rc::new(RefCell::new(None));
+        let c = closure.clone();
+
+        let end_transition = {
+            move || {
+                if let Some(element) = web_sys::window()
+                    .and_then(|window| window.document())
+                    .and_then(|document| document.query_selector(selector).unwrap())
+                {
+                    remove_class(&element, enter);
+                    remove_class(&element, enter_to);
+                    add_class(&element, entered);
                     if let Some(after_enter) = after_enter.as_ref() {
                         after_enter();
                     }
-                };
-
-                if let Some(before_enter) = before_enter.as_ref() {
-                    before_enter();
                 }
-
-                element.add_class(enter);
-                element.add_class(enter_from);
-
-                let _ = window.request_animation_frame({
-                    let element = element.clone();
-                    let enter_from = enter_from.clone();
-                    let enter_to = enter_to.clone();
-
-                    let boxed: Box<dyn Fn()> = Box::new(move || {
-                        element.remove_class(&enter_from);
-                        element.add_class(&enter_to);
-                        oneshot_event(&element, ev::transitionend, |_| end_transition());
-                        oneshot_event(&element, ev::animationend, |_| end_transition());
-                    });
-
-                    Closure::<dyn Fn()>::new::<Box<dyn Fn() + 'static>>(unsafe {
-                        mem::transmute(boxed)
-                    })
-                    .as_ref()
-                    .unchecked_ref()
-                });
-
-                initial = false;
             }
-        } else {
-            let Properties {
-                leave,
-                leave_from,
-                leave_to,
-                entered,
-                before_leave,
-                after_leave,
-                ..
-            } = properties;
+        };
 
-            if let Some(before_leave) = before_leave {
-                before_leave();
-            }
+        if let Some(before_enter) = before_enter.as_ref() {
+            before_enter();
+        }
 
-            element.remove_class(entered);
-            element.add_class(leave);
-            element.add_class(leave_from);
+        element.add_class(enter);
+        element.add_class(enter_from);
 
-            let _ = window.request_animation_frame({
-                let leave_from = leave_from.clone();
-                let leave_to = leave_to.clone();
-                let element = element.clone();
+        let _ = window.request_animation_frame({
+            let element = element.clone();
+            let enter_from = enter_from.clone();
+            let enter_to = enter_to.clone();
 
-                Closure::<dyn Fn()>::new(move || {
-                    element.remove_class(&leave_from);
-                    element.add_class(&leave_to);
-                })
-                .as_ref()
-                .unchecked_ref()
+            let boxed: Box<dyn Fn()> = Box::new(move || {
+                element.remove_class(&enter_from);
+                element.add_class(&enter_to);
+                oneshot_event(&element, ev::transitionend, |_| end_transition());
+                oneshot_event(&element, ev::animationend, |_| end_transition());
+                let _ = c.borrow_mut().take();
             });
 
-            let end_transition = move || {
-                element.remove_class(leave);
-                element.remove_class(leave_to);
+            *closure.borrow_mut() = Some(Closure::<dyn Fn()>::new::<Box<dyn Fn() + 'static>>(
+                unsafe { mem::transmute(boxed) },
+            ));
+
+            closure.borrow().as_ref().unwrap().as_ref().unchecked_ref()
+        });
+    };
+
+    #[cfg(target_arch = "wasm32")]
+    let leave = move |element: &DomNode, window: Window| {
+        let Properties {
+            leave,
+            leave_from,
+            leave_to,
+            entered,
+            before_leave,
+            after_leave,
+            ..
+        } = properties;
+
+        if let Some(before_leave) = before_leave {
+            before_leave();
+        }
+
+        element.remove_class(entered);
+        element.add_class(leave);
+        element.add_class(leave_from);
+
+        let closure = Rc::new(RefCell::new(None));
+        let c = closure.clone();
+
+        let _ = window.request_animation_frame({
+            let leave_from = leave_from.clone();
+            let leave_to = leave_to.clone();
+            let element = element.clone();
+
+            *closure.borrow_mut() = Some(Closure::<dyn Fn()>::new(move || {
+                element.remove_class(&leave_from);
+                element.add_class(&leave_to);
+                let _ = c.borrow_mut().take();
+            }));
+            closure.borrow().as_ref().unwrap().as_ref().unchecked_ref()
+        });
+
+        let end_transition = move || {
+            if let Some(element) = web_sys::window()
+                .and_then(|window| window.document())
+                .and_then(|document| document.query_selector(selector).unwrap())
+            {
+                remove_class(&element, leave);
+                remove_class(&element, leave_to);
                 visible.set(false);
                 if let Some(after_leave) = after_leave {
                     after_leave();
                 }
-            };
-            oneshot_event(element, ev::transitionend, move |_| end_transition());
-            oneshot_event(element, ev::animationend, move |_| end_transition());
-        }
+            }
+        };
+        oneshot_event(element, ev::transitionend, move |_| end_transition());
+        oneshot_event(element, ev::animationend, move |_| end_transition());
     };
 
+    #[cfg(target_arch = "wasm32")]
+    let mut initial = true;
+
     create_effect(cx, move || {
-        let should_show = props.show.get();
+        let show = props.show.clone();
+        let should_show = *create_selector(cx, move || show.get()).get();
 
         if should_show {
             visible.set(true);
@@ -207,8 +269,16 @@ pub fn Transition<'cx, G: Html>(cx: Scope<'cx>, props: TransitionProps<'cx, G>) 
 
         #[cfg(target_arch = "wasm32")]
         {
-            if let Some((window, node)) = web_sys::window().zip(node.try_get::<DomNode>()) {
-                transition(&node, should_show, window);
+            if !initial {
+                if let Some((window, node)) = web_sys::window().zip(node.try_get::<DomNode>()) {
+                    if should_show {
+                        enter(&node, window);
+                    } else {
+                        leave(&node, window);
+                    }
+                }
+            } else {
+                initial = false;
             }
         }
     });
@@ -228,9 +298,11 @@ pub fn Transition<'cx, G: Html>(cx: Scope<'cx>, props: TransitionProps<'cx, G>) 
     element.set_class_name((*class.get()).clone().into());
     element.set_children(cx, children);
     element.apply_attributes(cx, &props.attributes);
+    element.set_attribute("data-sh-transition-id".into(), id.clone().into());
 
     view! { cx,
         (if *visible.get() {
+            node.set(view.as_node().unwrap().clone());
             view.clone()
         } else {
             View::empty()
